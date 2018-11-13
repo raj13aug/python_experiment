58f522a4de53f1a435315af7a1b286583e4cdedf
Nataraj.R <raj13aug@gmail.com>
2018-11-13 22:55:04 +0530

    Example

--- string.py
+++ string.py
@@ -2,4 +2,20 @@
 # Strings (the str type)
 
 "couples".find('es')
+'''
+This for three quotestion
+'''
+
+print ("Hai")
+print ('raj')
+
+print ("Nat" + "araj")
+
+print ('raj' * 4)
+
+LOWER = 'RAJ'
+
+print (LOWER.lower())
+print ("Hai\\")
+print ("He\nello")
 
